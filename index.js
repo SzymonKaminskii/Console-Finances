f244abe38c656538c3c7d25820072f38d7f8226c
Szymon Kaminski <szymon.kaminskii@hotmail.com>
2022-11-27 18:26:11 +0000

    Added variables

--- index.js
+++ index.js
@@ -86,3 +86,8 @@ var finances = [
 ['Jan-2017', 138230],
 ['Feb-2017', 671099]
 ];
+var NumberOfMonths = finances.length;
+var sum = 0;
+var average = 0;
+var CurrentMonth = 0;
+var PreviousMonth = 0;
